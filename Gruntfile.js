1fb4e53d04b20175b624a8d09a0d6ea38c050449
Michel Fontes <mwfontes@gmail.com>
2017-10-10 15:30:50 -0300

    Adding first grunt modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,35 @@
 module.exports = function(grunt) {
-    
+    // grunt.initConfig({
+    //     pkg: grunt.file.readJSON("package.json"),
+    //     concat: {
+    //         js: {
+    //             src: ['src/js/1.js', 'src/js/2.js'],
+    //             dest: 'build/js/main.js'
+    //         },
+    //         css: {
+    //             src: ['src/css/1.scss', 'src/css/2.scss'],
+    //             dest: 'build/css/main.css'
+    //         }
+    //     },
+    //     watch: {
+    //         everything: {
+    //             files: ['src/**/*'],
+    //             tasks: ['concat']
+    //         }
+    //     }
+        
+    //     //,
+    //     // cssmin: {
+    //         //     combine: {
+    //             //         files: {
+    //                 //             "src/css/main.css": [""]
+    //                 //         }
+    //                 //     }
+    //                 // }
+    //     });
+        
+    //     grunt.loadNpmTasks('grunt-contrib-concat');
+    //     grunt.loadNpmTasks('grunt-contrib-watch');
+        
+    //     grunt.registerTask("default", ['concat', 'watch']);
 }
